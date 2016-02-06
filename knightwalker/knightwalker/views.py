89cb3d553ffe8d2f923b941bd5dfedb59aaf4b7e
rnvarma <rohan@prompthire.com>
2016-02-06 00:15:40 -0500

    added homepage and stuff

--- knightwalker/knightwalker/views.py
+++ knightwalker/knightwalker/views.py
@@ -5,4 +5,15 @@ from backend.models import *
 
 class HomePage(View):
 	def get(self, request):
-		return render(request, 'index.html')
+		user = request.user
+		if not UserData.objects.filter(user=user).count():
+			email = user.email
+			andrewID = email.split("@")[0]
+			name = user.first_name + " " + user.last_name
+			ud = UserData(user=user, andrewID=andrewID, name=name)
+			ud.save()
+		return render(request, 'home.html')
+
+class Login(View):
+	def get(self, request):
+		return render(request, 'login.html')
